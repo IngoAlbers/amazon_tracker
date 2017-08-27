1497f650c8d50eb4363d0f3f28a34cdb40edf83e
Ingo Albers <ingo.albers@live.de>
2017-08-27 14:12:46 +0200

    Store price after create

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ require 'amazon/ecs'
 class Article < ApplicationRecord
   has_many :prices
 
+  after_create :store_current_price
+
   def current_price
     res = Amazon::Ecs.item_lookup(asin, { response_group: 'Offers', country: 'de' })
     item = res.get_element('Item')
